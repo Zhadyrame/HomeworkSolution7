222f479b4d0343028564e2fe1ef9984a4cdb8a9d
Zhadyrame <zhadyra.nadirkhan@narxoz.kz>
2025-04-27 13:02:12 +0500

    Add ShuffleSeasonIterator for shuffled episode traversal with fixed seed

--- scr/Classes01/ShuffleSeasonIterator.java
+++ scr/Classes01/ShuffleSeasonIterator.java
@@ -1,1 +1,24 @@
-.
+import java.util.Collections;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class ShuffleSeasonIterator implements EpisodeIterator {
+    private List<Episode> shuffledEpisodes;
+    private int position = 0;
+
+    public ShuffleSeasonIterator(Season season) {
+        this.shuffledEpisodes = new ArrayList<>(season.getEpisodes());
+        Collections.shuffle(shuffledEpisodes, new Random(42)); // fixed seed
+    }
+
+    @Override
+    public boolean hasNext() {
+        return position < shuffledEpisodes.size();
+    }
+
+    @Override
+    public Episode next() {
+        return shuffledEpisodes.get(position++);
+    }
+}
