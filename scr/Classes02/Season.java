be31bafc07dab6ae774db42b0cfebc5472fee5b9
Zhadyrame <zhadyra.nadirkhan@narxoz.kz>
2025-04-27 12:39:46 +0500

    Add Season class with episode storage and addEpisode() method

--- scr/Classes02/Season.java
+++ scr/Classes02/Season.java
@@ -1,1 +1,19 @@
-.
+import java.util.ArrayList;
+import java.util.List;
+
+public class Season implements Iterable<Episode> {
+    private List<Episode> episodes = new ArrayList<>();
+
+    public void addEpisode(Episode e) {
+        episodes.add(e);
+    }
+
+    public List<Episode> getEpisodes() {
+        return episodes;
+    }
+
+    @Override
+    public java.util.Iterator<Episode> iterator() {
+        return new SeasonIterator(this);
+    }
+}
