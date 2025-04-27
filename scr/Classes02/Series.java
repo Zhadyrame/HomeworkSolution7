216a1ee47cf3fa0bfcfdb540de464d9300178232
Zhadyrame <zhadyra.nadirkhan@narxoz.kz>
2025-04-27 12:42:37 +0500

    Add Series class with addSeason() method

--- scr/Classes02/Series.java
+++ scr/Classes02/Series.java
@@ -1,1 +1,12 @@
-.
+import java.util.ArrayList;
+import java.util.List;
+public class Series {
+    private List<Season> seasons = new ArrayList<>();
+    public void addSeason(Season season) {
+        seasons.add(season);
+    }
+    public List<Season> getSeasons() {
+        return seasons;
+    }
+}
+
